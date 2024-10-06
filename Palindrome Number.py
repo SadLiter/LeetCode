46319c25d3f39a766df7f78fb98a67f111f9f8c6
SadLiter <valentinvolkovgg@gmail.com>
2024-10-07 00:12:46 +0300

    new file

---
+++ Palindrome Number.py
@@ -0,0 +1,10 @@
+def isPalindrome(num):
+    res = 0
+    answer = num
+    while num > 0:
+        res = res * 10 + num % 10
+        num //= 10
+
+    return res == answer
+
+print(isPalindrome(121))
