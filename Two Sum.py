42921284f0ac10c7047e83d1b8a9fa03db73b1be
SadLiter <valentinvolkovgg@gmail.com>
2024-10-06 23:59:27 +0300

    new file

---
+++ Two Sum.py
@@ -0,0 +1,9 @@
+def twoSum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in hashmap:
+            return [hashmap[complement], i]
+        hashmap[num] = i
+
+print(twoSum([2,7,11,15], 9))
